949e91ba3bb6aab850d1b130a91cbdcd1f73f001
Hector Fernandes <hectorhu17@gmail.com>
2022-06-24 22:05:24 +0200

    fix: develope in develop branch

--- minishell.c
+++ minishell.c
@@ -1,22 +1,2 @@
 #include "include/minishell.h"
 
-int	main(int argc, char **argv, char **envp)
-{
-	(void)argc;
-	(void)argv;
-	(void)envp;
-
-	char	*line;
-	char	*cmdline;
-
-	while(42)
-	{
-		line = readline(cmdline);
-		printf("%s\n", line);
-		if (!ft_strlen(line))
-			break;
-		free(cmdline);
-	}
-	free(line);
-	return (0);
-}
