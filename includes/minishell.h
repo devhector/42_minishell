358ecd6b5bdbc19f3a0c20a2b67b64d2f3b9f962
Hector Fernandes <hectorhu17@gmail.com>
2022-06-28 21:00:51 +0200

    chore: create includes folder

---
+++ includes/minishell.h
@@ -0,0 +1,12 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "libft.h"
+
+void	minishell(char	**envp);
+
+#endif
