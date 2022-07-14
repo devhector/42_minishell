58ce94d09fc647a8fc1e2a7320a36c088562b24d
Hector Fernandes <hectorhu17@gmail.com>
2022-07-14 21:29:38 +0200

    fix: accept >>|

--- src/tokenizer/tokens_checker.c
+++ src/tokenizer/tokens_checker.c
@@ -27,7 +27,7 @@ static int	check_redirections(char *str)
 	{
 		if (str[1] == '\0')
 			return (0);
-		else if (str[1] == '>' && str[2] != '\0')
+		else if (str[1] == '>' && !(str[2] == '\0' || str[2] == '|'))
 			return (1);
 	}
 	else if (str[0] == '<' && !(str[1] == '<' || str[1] == '\0'))
