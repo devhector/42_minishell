6bd5362dd66a193431a53b898249696daa30f6c5
Hector Fernandes <hectorhu17@gmail.com>
2022-07-14 21:30:32 +0200

    fix: now accept "-" "_" and "="

--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -32,13 +32,15 @@ static void	find_end_token(t_shell *hell, int *i, int isalnum)
 	str = hell->line;
 	if (isalnum)
 	{
-		while (str[*i] && !ft_isspace(str[*i]) && ft_isalnum(str[*i]))
+		while (str[*i] && !ft_isspace(str[*i]) && (ft_isalnum(str[*i])
+				|| str[*i] == '_' || str[*i] == '-' || str[*i] == '='))
 			*i += 1;
 		create_token(hell, start, *i);
 	}
 	else
 	{
-		while (str[*i] && !ft_isspace(str[*i]) && !ft_isalnum(str[*i]))
+		while (str[*i] && !ft_isspace(str[*i]) && !ft_isalnum(str[*i])
+			&& str[*i] != '\"' && str[*i] != '\'' && str[*i] != '|')
 			*i += 1;
 		create_token(hell, start, *i);
 	}
@@ -55,9 +57,14 @@ static void	get_tokens(t_shell	*hell)
 	{
 		if (ft_isspace(str[i]))
 			i++;
+		else if (str[i] == '|')
+		{
+			create_token(hell, i, i + 1);
+			i++;
+		}
 		else if (str[i] == '\'' || str[i] == '\"')
 			create_quote_token(hell, &i);
-		else if (!ft_isalnum(str[i]))
+		else if (!ft_isalnum(str[i]) && str[i] != '_' && str[i] != '-')
 			find_end_token(hell, &i, 0);
 		else
 			find_end_token(hell, &i, 1);
@@ -67,16 +74,26 @@ static void	get_tokens(t_shell	*hell)
 int	tokenizer(t_shell *hell)
 {
 	get_tokens(hell);
-	// t_list	*tmp;
-	// int		i = 0;
-	// tmp = hell->tokens;
-	// while (tmp)
-	// {
-	// 	printf("[%d] - %s\n", i, (char *)tmp->content);
-	// 	tmp = tmp->next;
-	// 	i++;
-	// }
-	// printf("tokenks: %d\n", ft_lstsize(hell->tokens));
+	t_list	*tmp;
+	int		i = 0;
+	tmp = hell->tokens;
+	while (tmp)
+	{
+		printf("[%d] - %s\n", i, (char *)tmp->content);
+		tmp = tmp->next;
+		i++;
+	}
+	printf("tokenks: %d\n", ft_lstsize(hell->tokens));
+	printf("---------\n");
+	if (tokens_checker(hell))
+		printf("syntax error: quotes or redirections\n");
+	tmp = hell->tokens;
+	while (tmp)
+	{
+		printf("[%d] - %s\n", i, (char *)tmp->content);
+		tmp = tmp->next;
+		i++;
+	}
 	ft_lstclear(&hell->tokens, free);
 	return (0);
 }
