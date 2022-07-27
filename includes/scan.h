aa502c05c24cc6af773d03fafb098464260840b2
Hector Fernandes <hectorhu17@gmail.com>
2022-07-27 11:43:14 -0300

    fix: move struct scan to scan.h

--- includes/scan.h
+++ includes/scan.h
@@ -1,6 +1,13 @@
 #ifndef SCAN_H
 # define SCAN_H
 
+typedef struct s_scan
+{
+	char	*token;
+	char	*type;
+	char	*error;
+} t_scan;
+
 int	is_quote(char c);
 int	is_redirect(char c);
 int	is_separator(char c);
