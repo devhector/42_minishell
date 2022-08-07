39bde58e0cf391b85ce79369b9b11b7d53bcf9de
Hector Fernandes <hectorhu17@gmail.com>
2022-08-07 17:13:42 -0300

    chore: now accept path

--- includes/command.h
+++ includes/command.h
@@ -6,9 +6,11 @@ typedef struct s_cmd
 	int		id;
 	int		fd_in;
 	int		fd_out;
+	int		pipe_d;
 	int		pipe[2];
 	int		is_piped;
 	char	**cmd_tab;
+	char	*path;
 	t_list	*command;
 	t_list	*redirect;
 } t_cmd;
