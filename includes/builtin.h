0dc162380aa95b46a37fb500e7e6a248ea4e1a17
Hector Fernandes <hectorhu17@gmail.com>
2022-08-13 15:37:24 -0300

    fix: resolve errors

--- includes/builtin.h
+++ includes/builtin.h
@@ -1,8 +1,10 @@
 #ifndef BUILTIN_H
 #define BUILTIN_H
 
-#include minishell.h
+#include "minishell.h"
 
 int	echo(char *args, int fd)
 int	env(t_hash_elem **table, int fd)
 int export(char *args, t_hash_elem **table)
+
+#endif
