8f1b6c2f2131e4a158c9a5e90e91d7c322dd8795
Hector Fernandes <hectorhu17@gmail.com>
2022-08-23 12:55:00 -0300

    fix: resolve error out

--- src/builtins/b_cd.c
+++ src/builtins/b_cd.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 17:47:59 by hectfern          #+#    #+#             */
-/*   Updated: 2022/08/23 12:38:39 by hectfern         ###   ########.fr       */
+/*   Updated: 2022/08/23 12:54:12 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,8 @@ int	cd_path(char *str, t_shell *hell)
 	new = getcwd(NULL, 0);
 	if (!new)
 	{
-		hell->error = ft_strdup("cd: Current directory not found");
+		ft_putendl_fd("minisHell: cd: Current directory not found", \
+				STDERR_FILENO);
 		return (1);
 	}
 	if (str[0] == '-' && str[1] == '\0')
