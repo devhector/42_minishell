45ed3193952f2d5c6f2f5a12236c65846b40bdb5
Hector Fernandes <hectorhu17@gmail.com>
2022-08-25 19:01:01 -0300

    feat: add parentesis syntax

--- src/syntax/syntax.c
+++ src/syntax/syntax.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 17:45:35 by hectfern          #+#    #+#             */
-/*   Updated: 2022/08/22 17:45:36 by hectfern         ###   ########.fr       */
+/*   Updated: 2022/08/25 18:58:56 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,27 @@ int	redirect_syntax(t_list *token)
 	return (0);
 }
 
+int	parentesis_syntax(t_list *token)
+{
+	int		i;
+	t_scan	*scan;
+
+	i = 0;
+	scan = (t_scan *)token->content;
+	while (scan->token[i])
+	{
+		if ((scan->token[i] == '(' || scan->token[i] == ')')
+			&& has_quote(scan->token) == -1)
+		{
+			scan->error = ft_strdup(" syntax error parentesis");
+			g_exit_code = 2;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 int	syntax(t_shell *hell)
 {
 	t_list	*token;
@@ -76,6 +97,8 @@ int	syntax(t_shell *hell)
 		if (pipe_syntax(token))
 			return (1);
 		if (redirect_syntax(token))
+			return (1);
+		if (parentesis_syntax(token))
 			return (1);
 		token = token->next;
 	}
